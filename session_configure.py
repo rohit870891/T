4bd61d04bb1c4ea59aca30ab0c9a16b7e87f00bb
rohit870891 <koyeb77user@gmail.com>
2025-02-07 01:22:37 +0530

    Update session_configure.py

--- session_configure.py
+++ session_configure.py
@@ -1,9 +1,9 @@
 import os
 from pyrogram import Client
 
-api_id = os.environ.get('22469064')
-api_hash = os.environ.get('c05481978a217fdb11fa6774b15cba32')
-bot_token = os.environ.get('7542241757:AAFgeHI2tr518Hbh8DOvLIfeCpQj0udfk-Y')
+api_id = os.environ.get('API_ID', '22469064')
+api_hash = os.environ.get('API_HASH', 'c05481978a217fdb11fa6774b15cba32')
+bot_token = os.environ.get('BOT_TOKEN', '7542241757:AAFgeHI2tr518Hbh8DOvLIfeCpQj0udfk-Y')
 app = Client(
     "teraBox-Bypass",
     api_id=api_id, api_hash=api_hash,
